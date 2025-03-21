33a867706adc83c2ab18889c332e08dc1d676d5e
tictools <jaumevillarreal@gmail.com>
2025-03-21 17:15:10 +0100

    feat: Extend vite config for tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: "jsdom",
+  },
+});
